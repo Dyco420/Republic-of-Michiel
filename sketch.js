fc8d6a2c17cfa196253e2271b7e8a96417ba11e4
Dyco420 <dylancoene@hotmail.com>
2021-12-26 02:26:08 +0100

    Spancer

--- sketch.js
+++ sketch.js
@@ -1,3 +1,7 @@
+function randomBetween(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
 let x;
 let y;
 let michiel;
@@ -5,14 +9,15 @@ let michiel;
 let xspeed;
 let yspeed;
 function preload(){
-  michiel = loadImage("https://raw.githubusercontent.com/Dyco420/Republic-of-Michiel/main/MichielH.png")
+  michiel = loadImage("https://raw.githubusercontent.com/Dyco420/Republic-of-Michiel/main/spancer.jpg")
 }
+
 function setup() {
-  createCanvas(800, 600);
-  x=400;
-  y=300;
-  xspeed=5;
-  yspeed=5;
+  createCanvas(1200, 800);
+  x=randomBetween(5,1200-165);
+  y=randomBetween(5,800-165);
+  xspeed=6;
+  yspeed=6;
 }
 
 function draw() {
@@ -23,10 +28,10 @@ function draw() {
   x = x + xspeed;
   y = y + yspeed;
   
-  if (x+160==width || x==0){
+  if (x+160>width || x<0){
     xspeed = xspeed*-1;
   }
-  if (y == 0 || y+160==height){
+  if (y < 0 || y+160>height){
     yspeed = yspeed*-1
   }
 }
